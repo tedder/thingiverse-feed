f4950405410fe172dbebbf19e3fcb77b65738ccb
tedder <ted.timmons@stansonhealth.com>
2018-06-14 12:14:06 -0700

    convert to mysql, pagination, etc

--- thingiverse-bow.py
+++ thingiverse-bow.py
@@ -41,10 +41,11 @@ def get_secret(secret_name):
   sc_ret = sc.get_secret_value( SecretId=secret_name )
 
   if 'SecretString' in sc_ret:
+    if DEBUG > 12: print(sc_ret)
     secret = sc_ret['SecretString']
     return secret
   else:
-    if DEBUG: print(sc_ret)
+    if DEBUG > 11: print(sc_ret)
     binary_secret_data = sc_ret['SecretBinary']
     return binary_secret_data
 
@@ -57,16 +58,20 @@ class ThingDB:
 
   def __init__(self):
     self.db = None
+    self.dbsec = None
     self.get_db()
 
   def have_column(self, table, column):
-    d = self.get_db()
-    c = self.db.cursor()
-    c.execute('SELECT {} FROM {} LIMIT 1'.format(column, table))
-    c.fetchone()
-    # must be okay
-    return True
+    try:
+      d = self.get_db()
+      c = self.db.cursor()
+      c.execute('SELECT {} FROM {} LIMIT 1'.format(column, table))
+      c.fetchone()
+      # must be okay
+      return True
 
+    except sqlite3.OperationalError:
+      return None # this is expected
 
   def add_column(self, table, column, column_def):
     if self.have_column(table, column):
@@ -77,12 +82,26 @@ class ThingDB:
     d.commit()
 
   def get_db(self):
-    if self.db and self.db.ping(reconnect=True):
+    # versions needed:
+    # >= 3.3 for 'if not exists'
+    #print(sqlite3.sqlite_version) -> '3.18.0'
+    #print(sqlite3.sqlite_version_info) -> (3, 18, 0)
+
+    if self.db:
+      # ping throws, so we don't actually care what it returns
+      self.db.ping(reconnect=True)
       return self.db
 
-    dbsec = json.loads(get_secret('serverless_mysql_1'))
-    self.db = pymysql.connect(host=dbsec['host'], user=dbsec['username'], password=dbsec['password'],
-      autocommit = True, db='thingiverse', cursorclass=pymysql.cursors.DictCursor, charset='utf8mb4')
+    self.dbsec = json.loads(get_secret('serverless_mysql_1'))
+    self.db = pymysql.connect(host=self.dbsec['host'], user=self.dbsec['username'], password=self.dbsec['password'],
+      autocommit = True, db='', cursorclass=pymysql.cursors.DictCursor, charset='utf8mb4')
+
+    with warnings.catch_warnings():
+      warnings.simplefilter("ignore")
+      c = self.db.cursor()
+      c.execute('CREATE DATABASE IF NOT EXISTS thingiverse')
+      self.db.commit()
+    self.db.select_db('thingiverse')
 
     with warnings.catch_warnings():
       warnings.simplefilter("ignore")
@@ -99,7 +118,7 @@ class ThingDB:
     c = self.get_db().cursor()
     c.execute('''SELECT first_seen FROM items WHERE itemid=%s''', (int(itemid),) )
     dt = c.fetchone()
-    if DEBUG: print(dt)
+    if DEBUG > 1: print(dt)
 
     # might be none, which is okay.
     if not dt: return None
@@ -118,11 +137,102 @@ class ThingDB:
     if not row.get('first_seen'):
       #row['first_seen'] = datetime.now(timezone.utc).replace(microsecond=0).isoformat() # py3.6 timespec='seconds')
       row['first_seen'] = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
-    if DEBUG: print(row['first_seen'])
+    if DEBUG > 1: print(row['first_seen'])
     #print("a", {**row})
     c.execute('''INSERT INTO items
       (itemid,  title,  first_seen) VALUES
       (%(itemid)s, %(title)s, %(first_seen)s)
+      ''', {**row})
+    self.get_db().commit()
+    return row['first_seen']
+
+
+class ThingDB_sqlite:
+
+  def close(self):
+    if self.db:
+      self.db.commit()
+      self.db.close()
+      self.db = None
+
+  def __init__(self):
+    self.db = None
+    self.get_db()
+
+  def have_column(self, table, column):
+    try:
+      d = self.get_db()
+      c = self.db.cursor()
+      c.execute('SELECT {} FROM {} LIMIT 1'.format(column, table))
+      c.fetchone()
+      # must be okay
+      return True
+
+    except sqlite3.OperationalError:
+      return None # this is expected
+
+  def add_column(self, table, column, column_def):
+    if self.have_column(table, column):
+      return # nothing to do
+    d = self.get_db()
+    c = self.db.cursor()
+    c.execute('ALTER TABLE {} ADD COLUMN {} {}'.format(table, column, column_def))
+    d.commit()
+
+  def get_db(self):
+    # versions needed:
+    # >= 3.3 for 'if not exists'
+    #print(sqlite3.sqlite_version) -> '3.18.0'
+    #print(sqlite3.sqlite_version_info) -> (3, 18, 0)
+
+    if self.db:
+      return self.db
+
+    pypath = os.path.dirname(__file__)
+    dbpath = os.path.join(pypath, 'thingiverse-bom.sqlite3')
+    #print("path: {}".format(dbpath))
+    if not os.path.isfile(dbpath):
+      raise Exception("we expect our sqlite3 db to exist ({}). If it doesn't, maybe it's in the wrong place?".format(dbpath))
+    self.db = sqlite3.connect(dbpath)
+    c = self.db.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS items
+      (itemid NUMBER, title TEXT, first_seen TEXT)
+      ''')
+    #c.execute('''CREATE TABLE IF NOT EXISTS state (state_name TEXT, state_value TEXT)''');
+    self.db.commit()
+    #self.add_column('spots', 'band', 'NUMBER')
+    return self.db
+
+  def item_seen(self, itemid):
+    #print("iid: {}".format(type(itemid)))
+    c = self.get_db().cursor()
+    c.execute('''SELECT first_seen FROM items WHERE itemid=?''', (int(itemid),) )
+    ret = c.fetchone()
+
+    # might be none, which is okay.
+    if not ret: return None
+    dt = ret[0]
+
+    # might be none, which is okay.
+    if not dt: return None
+
+    #print("dt at db: ", dt)
+    return dt
+    #dtp = dtparser.parse(dt).replace(tzinfo=timezone.utc)
+    #print("post parse: ", dtp)
+    #return dtp
+
+  def insert_item(self, row):
+    c = self.get_db().cursor()
+    s = self.item_seen(row['itemid'])
+    if s: return s
+
+    if not row.get('first_seen'):
+      row['first_seen'] = datetime.now(timezone.utc).replace(microsecond=0).isoformat() # py3.6 timespec='seconds')
+    #print("a", {**row})
+    c.execute('''INSERT INTO items
+      (itemid,  title,  first_seen) VALUES
+      (:itemid, :title, :first_seen)
       ''', {**row})
     self.get_db().commit()
     return row['first_seen']
@@ -181,15 +291,29 @@ def make_request(url, params={}):
     raise Exception("could not get auth_token from secrets manager: {}".format(auth_token))
 
   req = requests.get(url, headers={'Authorization': 'Bearer {}'.format(auth_token)}, params=params)
+  #print(req.headers['link'])
+  #sys.exit(0)
+
   return req.json()
 
 def get_recent_collection_items(db, apiurl, collection_name):
   retitems = []
-  for thing in make_request(apiurl + "/things", params={'sort': 'date', 'order': 'desc'}):
+
+  things = []
+  pagenum = 1
+  while True:
+    this_set = make_request(apiurl + "/things", params={'sort': 'date', 'order': 'desc', 'page': pagenum})
+    if DEBUG: print("page: ", pagenum, " set size: ", len(this_set))
+    if len(this_set) == 0:
+      break
+    things.extend(this_set)
+    pagenum += 1
+
+  for thing in things:
     #print("xx", json.dumps(itemjson, indent=2))
     #thing["url"]: "https://api.thingiverse.com/things/2788192",
     #row['first_seen]' = datetime.now.isoformat() # py3.6 timespec='seconds')
-    if DEBUG: print(int(thing['id']), thing['name'])
+    if DEBUG > 1: print(int(thing['id']), thing['name'])
     seen = db.insert_item({'itemid': int(thing['id']), 'title': thing['name']})
     #seen_dt = seen.replace(tzinfo=timezone.utc)
     seen_dt = parsedate(seen).replace(tzinfo=timezone.utc)
@@ -226,9 +350,22 @@ def get_recent_collection_items(db, apiurl, collection_name):
 db = ThingDB()
 s3key = 'rss/thingiverse_kryptonicloser_recent_collected.json'
 items = []
-for collection in make_request(BASE_URL):
+
+collections = []
+pagenum = 1
+while True:
+  if DEBUG > 1: print("requesting page: ", pagenum)
+  this_collection_set = make_request(BASE_URL, {'page': pagenum})
+  if DEBUG: print("page: ", pagenum, " set size: ", len(this_collection_set))
+  if len(this_collection_set) == 0:
+    break
+  collections.extend(this_collection_set)
+  pagenum += 1
+
+for collection in collections:
+  #print(collection)
   last_modified = parsedate(collection['modified'])
-  if DEBUG: print(last_modified, datetime.now(timezone.utc))
+  if DEBUG: print(collection.get('Name'), last_modified, datetime.now(timezone.utc))
   if last_modified.replace(tzinfo=timezone.utc) < datetime.now(timezone.utc)-timedelta(days=14):
     if DEBUG: print("too old")
     continue
